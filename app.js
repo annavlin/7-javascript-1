539c382a1a190c9d26fbf4848687906b94c588d3
annavlin <Anechka211299@yandex.ru>
2025-03-26 23:44:44 +0700

    Тернарные операторы доп

--- app.js
+++ app.js
@@ -2,22 +2,31 @@ const bmwX3Price = 100000;
 const fordFocusPrice = 10000;
 const budget = 20000;
 
-let message;
+// let message;
 
-if (budget > bmwX3Price) {
-  message = 'BMW';
-} else if (budget > fordFocusPrice) {
-  message = 'Ford';
-} else {
-  message = 'Велосипед';
-}
+// if (budget > bmwX3Price) {
+//   message = 'BMW';
+// } else if (budget > fordFocusPrice) {
+//   message = 'Ford';
+// } else {
+//   message = 'Велосипед';
+// }
 
-10 > 0 ? console.log('больше 0') : console.log('не больше');
-// Это равносильно между собой
-if (10 > 0) {
-  console.log('больше 0');
-} else {
-  console.log('не больше');
-}
+// 10 > 0 ? console.log('больше 0') : console.log('не больше');
+// // Это равносильно между собой
+// if (10 > 0) {
+//   console.log('больше 0');
+// } else {
+//   console.log('не больше');
+// }
+// // также укороченный вариант
+// const str = 10 > 0 ? 'больше 0' : 'не больше';
+// console.log(str);
 
-console.log(` я хочу купить ${message}`);
+// // укороченный вариант написания верхней записи
+// console.log(` я хочу купить ${budget > bmwX3Price ? 'BMW' : 'Велосипед'}`);
+
+let message =
+  budget > bmwX3Price ? 'BMW' : budget > fordFocusPrice ? 'Ford' : 'Велосипед';
+
+console.log(`Я хочу купить ${message}`);
