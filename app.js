bb657937d310781f39bf9c8a5f6fa6539bbda5e5
annavlin <Anechka211299@yandex.ru>
2025-03-26 22:33:53 +0700

    JS начало

---
+++ app.js
@@ -0,0 +1,14 @@
+const secretNumber = '7';
+
+if (Number(secretNumber) === 7) {
+  console.log('Угадал строго');
+}
+
+if (Number(secretNumber) == 7) {
+  console.log('Угадал не строго');
+}
+
+const q = prompt('Введите число');
+if (q == 7) {
+  console.log('Угадал не строго');
+}
