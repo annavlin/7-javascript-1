c4cd3a3f795c3a31e7da1ebef91ac71fa3fafc4a
annavlin <Anechka211299@yandex.ru>
2025-04-13 00:53:28 +0700

    for of и for in

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const arr = [1, 4, 8, 7];
 
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 5) {
-    break;
-  }
   console.log(arr[i]);
 }
 
-let i = 0;
-while (arr[i] <= 5 && i < arr.length) {
-  console.log(arr[i]);
-  i++;
+for (let element of arr) {
+  console.log(element);
 }
 
-let j = 0;
-do {
-  console.log(j);
-  j++;
-} while (j < 0);
+for (let index in arr) {
+  console.log(arr[index]);
+}
