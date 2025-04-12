debb86ae1dd6202f95fcccd1874e44f36ef6a122
annavlin <Anechka211299@yandex.ru>
2025-04-13 00:30:36 +0700

    про циклы

--- app.js
+++ app.js
@@ -1,102 +1,29 @@
-// Массивы
-const users = ['Аня', 'Вика', 'Катя'];
-console.log(users);
-users[2] = 'Кристина';
-console.log(users);
-// users[4] = 'Никита';
-// console.log(users);
+// Цикл в цикле
 
-const arrLenght = users.push('Никита');
-console.log(users);
-console.log(arrLenght);
-
-users.unshift('Вася'); //добавляет в начало, смещая другие
-console.log(users);
-
-users.pop(); //удаляет массив
-console.log(users);
-
-const el2 = users.shift();
-console.log(el2);
-console.log(users);
-
-// slice метод (отрезает кусок, выборка от того числа, которое мы указали и если есть второе то до второго числа-второй элемент не включается)
-
-const roles = ['user', 'admin', 'manager', 'superuser'];
-
-const res1 = roles.slice(2, 3);
-console.log(roles);
-console.log(res1);
-
-const res2 = roles.slice(0, 2);
-console.log(res2);
-
-const res3 = roles.slice(-1);
-console.log(res3);
-
-const res4 = roles.slice(1, -1); //при отрицательном индексе считаем с конца
-console.log(res4);
-
-//splice
-
-const res5 = roles.splice(2);
-console.log(res5);
-console.log(roles);
-
-//reverse
-
-const res8 = roles.reverse();
-console.log(res8);
-
-//concat (добавляется к старому массиву новый)
-
-const newRoles = ['sisadmin', 'developer'];
-const res9 = roles.concat(newRoles);
-console.log(res9);
-
-/* 
-Дан список задач
-const tasks = ['Задача 1'];
-Сделать функции:
-- Добавление задачи в конец
-- Удаление задачи по названию
-- Перенос задаси в начало списка по названию 
-Всегда меняем исходный массив
- */
-const tasks = ['Задача 1', 'Задача 2'];
-
-function addToEnd(task) {
-  tasks.push(task);
-}
-addToEnd('Задача 3');
-console.log(tasks);
-
-function deleteTaskByName(taskName) {
-  console.log(tasks.indexOf(taskName));
-  const index = tasks.indexOf(taskName);
-  if (index >= 0) {
-    tasks.splice(index, 1);
+for (let i = 1; i < 5; i++) {
+  console.log(`Цикл 1 - ${i}`);
+  for (let j = 1; j < 5; j++) {
+    console.log(`Цикл 2 - ${j}`);
   }
 }
-deleteTaskByName('Задача 2');
-console.log(tasks);
 
-function moveToStart(taskName) {
-  const index = tasks.indexOf(taskName);
-  deleteTaskByName(taskName);
-  if (index >= 0) {
-    tasks.unshift(taskName);
+const tasks2 = [
+  [1, 'Задача 1'],
+  [2, 'Задача 2'],
+];
+
+for (let i = 0; i < tasks2.length; i++) {
+  for (let j = 0; j < tasks2[i].length; j++) {
+    console.log(tasks2[i][j]);
   }
 }
-moveToStart('Задача 3');
-console.log(tasks);
 
-let res = 1;
-function toPower(num, power) {
-  res = num ** power;
-  return res;
+for (let i = 1; i < 5; i++) {
+  console.log(`Вывод - ${i}`);
 }
 
-console.log(toPower(2, 3));
-res = 99;
-console.log(res);
+let i = 1;
+while (i < 5) {
+  console.log(`Вывод - ${i}`);
+  i++;
+}
