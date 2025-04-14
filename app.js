45edc4465211c4a0c49925dabbdec0f6f30b185c
annavlin <Anechka211299@yandex.ru>
2025-04-14 18:41:16 +0700

    Задача с reduce

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-const operations = [100, -20, 7, -30, 50];
+/*
+Найти среднее значение последовательность числе с помощью reduce */
 
-let balance = 0;
-for (const operation of operations) {
-  balance += operation;
-}
+const arr = [2, 4, 4, 10];
 
-console.log(balance);
-
-const finalBalance = operations.reduce((acc, operation, i) => {
-  console.log(`Итерация ${i}, acc: ${acc}, operation ${operation}`);
-  return (acc += operation);
-}, 0);
-
-console.log(finalBalance);
-
-const minElement = operations.reduce((acc, operation, i) => {
-  if (operation > acc) {
-    return acc;
+const avg = arr.reduce((acc, el, i) => {
+  if (i != arr.length - 1) {
+    return acc + el;
   }
   {
-    return operation;
+    return (acc + el) / arr.length;
   }
 }, 0);
-console.log(minElement);
-
-// 0 - acc = 0, operation = 100
-// 0 - acc = 100, operation = -20
+console.log(avg);
