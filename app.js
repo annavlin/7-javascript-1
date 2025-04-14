dcb67c2f2935d8249a3fa2b622aede1c1149a956
annavlin <Anechka211299@yandex.ru>
2025-04-14 20:37:57 +0700

    Решение задачи с find, findIndex, some

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+/*
+Написать функцию, которая возвращает true, если элемент есть, и false, если нет */
+
 const arr = [2, 4, 4, 10, 20];
-
-let elGT5;
-// for (const el of arr) {
-//   if (el > 5) {
-//     elGT5 = el;
-//     break;
+// function some(arr, el) {
+//   let result = arr.findIndex((element) => element === el);
+//   if (result < 0) {
+//     return false;
 //   }
+//   return true;
 // }
-// console.log(elGT5);
 
-elGT5 = arr.find((el) => el > 5);
-elGT5Index = arr.findIndex((el) => el > 5);
+// console.log(some(arr, 10));
 
-console.log(elGT5);
-console.log(elGT5Index); /* показывает индекс массива */
+function some(array, element) {
+  const res = array.find((el) => el === element);
+  return res == undefined ? false : true;
+}
+
+console.log(some(arr, 10));
+console.log(arr.some((el) => el === 10));
