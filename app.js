77141902d7d430cb22c3e877d03c10606d1b661d
annavlin <Anechka211299@yandex.ru>
2025-04-16 16:03:00 +0700

    Быстрое создание массивов, урок 70

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
-const users = ['Вася', 'Маша', 'Катя', 'Аня'];
-console.log(users);
-users.sort();
-console.log(users);
-//в данном случае sort сортирует массивы по алфавиту
+const arr = [1, 2, 3, 4, 5];
+console.log(new Array(1, 2, 3, 4, 5));
+const arr2 = new Array(5);
+console.log(arr2);
+arr2.fill(1, 0, 3); // тут результат будет [1, 1, 1, , ]
+arr2.fill(2, 3, 5); // тут добавляется, что с 3 по 5 элемеенты массива ставим 2б будет [1, 1, 1, 2, 2]
+console.log(arr2);
 
-const operations = [100, -300, -100, 50, 480];
-console.log(operations);
-
-// < 0 - a, b -сохраняем порядок
-// > 0 - b,a- меняем порядок
-
-operations.sort((a, b) => {
-  if (a > b) {
-    return 1;
-  }
-  if (a < b) {
-    return -1;
-  }
-});
-console.log(operations);
-
-// дальше короткий вариант верхней записи
-operations.sort((a, b) => a - b);
-console.log(operations);
-
-operations.sort((a, b) => {
-  if (a < b) {
-    return 1;
-  }
-  if (a > b) {
-    return -1;
-  }
-});
-console.log(operations);
+const arr3 = Array.from({ length: 5 }, (cur, i) => i + 1);
+console.log(arr3);
