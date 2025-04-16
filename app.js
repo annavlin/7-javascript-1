643c6bc9f7c3bd056f20300687d22aa1a2a398f1
annavlin <Anechka211299@yandex.ru>
2025-04-17 01:28:38 +0700

    Решение задачи на проверку номера (моя версия)

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-const str = 'Вася Пупкина';
-console.log(str.includes('a'));
-console.log(str.startsWith('В')); /* ищет слово, которое начинается с 'В' */
-console.log(str.endsWith('н')); /* ищет слово, которое заканчивается на 'н' */
+/* проверить является ли это номером телефона России */
+/* верные */
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = '  +7(910) 323-53-56  ';
 
-console.log(str.toLowerCase()); /* пишет все строчными буквами */
-console.log(str.toUpperCase()); /* пишет КАПСОМ*/
-console.log(str.replace('В', 'Ф'));
-/* меняет буквы местами 'Фася Пупкин'
- */
-console.log(str.replaceAll('а', 'и')); /* меняет ВСЕ буквы А на букву И */
+/*  неверные */
 
-const str2 = ' Вася Пупкина   \n';
-console.log(str2.trimStart()); /* убирает пробелы с начала */
-console.log(str2.trimEnd()); /* убирает пробелы с конца */
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
+
+function checkPhoneNumber(phoneNumber) {
+  console.log(phoneNumber);
+  console.log(Number(phoneNumber));
+  const res = phoneNumber
+    .replaceAll('+', '')
+    .replaceAll('(', '')
+    .replaceAll(')', '')
+    .replaceAll(' ', '')
+    .replaceAll('-', '');
+  if (isNaN(Number(res))) {
+    return false;
+  }
+  if (res.length !== 11) {
+    return false;
+  }
+  if (res.startsWith('8') || res.startsWith('7')) {
+    return true;
+  }
+  return false;
+}
+
+console.log(checkPhoneNumber(num4Error));
