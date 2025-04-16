a95b8aee7e13dae981e71745696493ed6f54bd95
annavlin <Anechka211299@yandex.ru>
2025-04-17 01:41:16 +0700

    Решение задачи на проверку номера (не моя версия)

--- app.js
+++ app.js
@@ -13,25 +13,26 @@ const num2Error = '+7d910d323-53-56';
 const num3Error = '9+7103235356';
 const num4Error = '89103g35356';
 
-function checkPhoneNumber(phoneNumber) {
-  console.log(phoneNumber);
-  console.log(Number(phoneNumber));
-  const res = phoneNumber
-    .replaceAll('+', '')
-    .replaceAll('(', '')
-    .replaceAll(')', '')
-    .replaceAll(' ', '')
-    .replaceAll('-', '');
-  if (isNaN(Number(res))) {
+function isPhoneNumber(num) {
+  num = num.trim();
+  num = num.replace('+7', '8');
+  if (!num.startsWith('8')) {
     return false;
   }
-  if (res.length !== 11) {
+  num = num.replaceAll('(', '');
+  num = num.replaceAll(')', '');
+  num = num.replaceAll(' ', '');
+  num = num.replaceAll('-', '');
+  if (num.length != 11) {
     return false;
   }
-  if (res.startsWith('8') || res.startsWith('7')) {
-    return true;
+  let onlyNumber = true;
+  for (const char of num) {
+    if (isNaN(Number(char))) {
+      onlyNumber = false;
+      break;
+    }
   }
-  return false;
+  return onlyNumber;
 }
-
-console.log(checkPhoneNumber(num4Error));
+console.log(isPhoneNumber(num4Error));
