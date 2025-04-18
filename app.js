756476ce5cd5e2e3b2522d742aa013d73b4ab897
annavlin <Anechka211299@yandex.ru>
2025-04-18 23:12:00 +0700

    Решение задачи по маскировке номера карты

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const film = 'Звездные Воины ';
-console.log(film.padStart(50, '⭐️'));
-console.log(film.padEnd(50, '⭐️'));
-console.log(film.repeat(10));
+/* замаскировать все, кроме последних 4х символов */
+/* *************8353 */
+
+const card = '2342834503458353';
+console.log(card.slice(-4).padStart(16, '*'));
