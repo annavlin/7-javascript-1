347315386c02509e8c4c52d7efaae974e3288884
annavlin <Anechka211299@yandex.ru>
2025-04-23 16:23:23 +0700

    Различие примитивов и обьектов

--- app.js
+++ app.js
@@ -1,68 +1,17 @@
-/* 
-Сделать обьект склад с методами добавления на склад, поиска по складку товара и расчет веса */
+let firstName = 'Anton';
+let firstName2 = firstName;
+firstName2 = 'New';
+console.log(firstName);
+console.log(firstName2);
 
-const warehouse = {
-  goods: [],
-  findGoodById: function (id) {
-    return this.goods.find((g) => g.id == id);
-  },
-  addGood: function (good) {
-    const existedGood = this.findGoodById(good.id);
-    if (existedGood) {
-      console.log('Этот товар есть на складе');
-      return;
-    }
-    this.goods.push(good);
-  },
+// в консоле в данном случае выводится
+//                                    Anton
+//                                    New
 
-  getWeightKg: function () {
-    return this.goods.reduce(
-      (acc, el) => (acc += el.weight?.kg ? el.weight?.kg : 0),
-      0
-    );
-  },
+const user1 = {
+  name: 'Anton',
 };
-
-/* товары */
-
-const car = {
-  id: 1,
-  weight: {
-    kg: 1000,
-  },
-  brand: 'Ford',
-};
-
-const chair = {
-  id: 2,
-  weight: {
-    kg: 2,
-  },
-};
-const paper = {
-  id: 3,
-  color: 'red',
-};
-
-const testt = {
-  id: 4,
-  color: 'blue',
-  weight: {
-    lb: 6,
-  },
-};
-
-warehouse.addGood(car);
-
-warehouse.addGood(chair);
-warehouse.addGood(paper);
-warehouse.addGood(testt);
-
-console.log(warehouse.goods);
-let findedItem = warehouse.findGoodById(6);
-console.log(findedItem);
-findedItem = warehouse.findGoodById(1);
-console.log(findedItem);
-
-const w = warehouse.getWeightKg();
-console.log(w);
+const user2 = user1;
+user2.name = 'New';
+console.log(user1);
+console.log(user2);
