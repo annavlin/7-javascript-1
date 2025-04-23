5d38b678f162dda3bc1e48829327999ab9c1cba0
annavlin <Anechka211299@yandex.ru>
2025-04-23 22:34:32 +0700

    Примитивы и обьекты- различия

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-let firstName = 'Anton';
-let firstName2 = firstName;
-firstName2 = 'New';
-console.log(firstName);
-console.log(firstName2);
-
-// в консоле в данном случае выводится
-//                                    Anton
-//                                    New
-
-const user1 = {
+const user = {
   name: 'Anton',
+  id: 1,
+  roles: ['Admin'],
 };
-const user2 = user1;
-user2.name = 'New';
-console.log(user1);
-console.log(user2);
+
+// const newUser = Object.assign({}, user);
+// user.name = 'NewUser';
+// console.log(user);
+// console.log(newUser);
+// в данном случае будет выводится NewUser     Anton
+
+const newUser2 = {
+  ...user /* это называется СПРЕД */,
+};
+newUser2.name = 'NewUser';
+newUser2.roles.push('User');
+console.log(user);
+console.log(newUser2);
+// в данном случае это аналогичная запись вышесказанному
