b02b0c1f3c47a4fb038eb37a534c5150f8abc7a8
annavlin <Anechka211299@yandex.ru>
2025-04-28 23:24:46 +0700

    Bind

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
 'use strict';
 
+// с помощью bind связываем контекст обьекта с функцией
+
 const audi = {
   make: 'Audi',
   model: 'A3',
-  year: 2021,
   damages: [],
+};
+
+const carManipulation = {
   addDamage(part, rate) {
-    console.log(
-      `У авто ${this.make} ${this.model} ${this.year} добавлено: повреждение ${part} со степенью ${rate}`
-    );
-    this.damages.push({
-      part,
-      rate,
-    });
+    this.damages.push({ part, rate });
+    console.log(`Добавить повреждение на ${this.make} ${this.model}`);
   },
 };
-// audi.addDamage('Капот', 1);
 
-const bmw = {
-  make: 'BMW',
-  model: 'X5',
-  year: 2022,
-  damages: [],
-};
-bmw.addDamage = audi.addDamage;
-// bmw.addDamage('Бампер', 2);
-const addDamageFunc = audi.addDamage;
-// addDamageFunc.call('Бампер', 2);
-addDamageFunc.call(bmw, 'Бампер', 2);
-addDamageFunc.call(audi, 'Бампер', 3);
-
-//call - вызывает, "вызови пожалуйста такую-то функцию на каком this и какой аргумент"
-
-//apply - применить метод - указываем аргумент и массив
-addDamageFunc.apply(bmw, ['Бампер', 2]);
-
-// РАБОТАЮТ ПРИМЕРНО ОДИНАКОВО, ТОЛЬКО ЭПЛАЙ С МАССИВАМИ
+const addDamageAudi = carManipulation.addDamage.bind(audi);
+addDamageAudi('Крыло', 3);
+console.log(audi);
