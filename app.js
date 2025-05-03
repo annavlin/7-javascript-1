67b19d22f5aa5964092b18b7e16d171e63a44e0f
annavlin <Anechka211299@yandex.ru>
2025-05-03 19:01:23 +0700

    Local Storage

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 'use strict';
 
-// document.querySelector('.button').addEventListener('click', function () {
-//   const input = document.querySelector('.input').value;
-//   if (!input) {
-//     return;
-//   }
-//   document.querySelector('.panel').innerText = input;
-//   document.querySelector('.input').value = '';
-// });
-
-// записываем эту функцию но по -другому
-
 function submitForm() {
   const input = document.querySelector('.input').value;
   if (!input) {
@@ -18,32 +7,19 @@ function submitForm() {
   }
   document.querySelector('.panel').innerText = input;
   document.querySelector('.input').value = '';
-  //   document.querySelector('.notification').classList.add('notification_active');
-  //   document
-  //     .querySelector('.notification')
-  //     .classList.remove('notification_hidden');
-  // }
-  console.log(document.querySelector('.notification').getAttribute('class'));
-  document.querySelector('.notification').setAttribute('class', 'notification');
-  document.querySelector('.notification').setAttribute('key', 1);
-  document.querySelector('.notification').setAttribute('user-id', 1);
-  console.log(
-    Number(document.querySelector('.notification').getAttribute('user-id'))
-  );
-}
 
-function inputChanged(e) {
-  if (e.code == 'Enter') {
-    submitForm();
+  function inputChanged(e) {
+    if (e.code == 'Enter') {
+      submitForm();
+    }
   }
 }
 
-const panelText = 'Панель';
-const panelClass = 'button';
-const newElement = document.createElement('div'); //создать новый элемент
-// устанавливаем атрибуты и классы
-newElement.setAttribute('user-id', 1);
-newElement.classList.add('panel'); // добавляем css-класс
-// newElement.innetHML = 'Кнопка';
-newElement.innerHTML = `<button class="${panelClass}">${panelText} </button>`;
-document.querySelector('.test').appendChild(newElement);
+localStorage.setItem('token', 'vnfknv');
+
+localStorage.setItem('token1', 1);
+localStorage.setItem('token1', true);
+const token1 = localStorage.getItem('token1');
+console.log(typeof token1);
+localStorage.removeItem('token1');
+localStorage.clear();
