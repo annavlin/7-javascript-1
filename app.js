8d56d6e4c96389d4e4977dad90c3bf2ce8ecb387
annavlin <Anechka211299@yandex.ru>
2025-05-03 18:18:36 +0700

    Добавление HTML на лету

--- app.js
+++ app.js
@@ -37,21 +37,13 @@ function inputChanged(e) {
     submitForm();
   }
 }
-// document.querySelector('.input').addEventListener('.keydown', (e) => {
-//   if (e.code == 'Enter') {
-//     submitForm();
-//   }
-// });
 
-function getContentElement() {
-  const content1 = document.querySelectorAll('.one>span')[0].innerText;
-  console.log(content1);
-  const content2 = document.querySelectorAll('.one>span')[1].innerText;
-  console.log(content2);
-  const content3 = document.querySelector('#two').innerText;
-  console.log(content3);
-  const content4 = document.querySelector('span[user-id="4"]').innerText;
-  console.log(content4);
-}
-
-getContentElement();
+const panelText = 'Панель';
+const panelClass = 'button';
+const newElement = document.createElement('div'); //создать новый элемент
+// устанавливаем атрибуты и классы
+newElement.setAttribute('user-id', 1);
+newElement.classList.add('panel'); // добавляем css-класс
+// newElement.innetHML = 'Кнопка';
+newElement.innerHTML = `<button class="${panelClass}">${panelText} </button>`;
+document.querySelector('.test').appendChild(newElement);
